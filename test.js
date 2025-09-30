60d083ed6ccf504710456e2c97c31152251cd449
squares2 <strategyquant8@gmail.com>
2025-09-30 23:20:13 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+	const firebase=require('firebase/app');
+	  const firebaseConfig = 
+	  {
+	    apiKey: "AIzaSyA2HIsidl_tWRatVbnPg_URSb3RAlP22d0",
+	    authDomain: "squares-a94b3.firebaseapp.com",
+	    databaseURL: "https://squares-a94b3-default-rtdb.firebaseio.com",
+	    projectId: "squares-a94b3",
+	    storageBucket: "squares-a94b3.firebasestorage.app",
+	    messagingSenderId: "112013960393",
+	    appId: "1:112013960393:web:592ccc05f01274da081918",
+	    measurementId: "G-K1FW15CXEC"
+	  };
+
+	  // Initialize Firebase
+	  const app = initializeApp(firebaseConfig);
+	  alert("good");
