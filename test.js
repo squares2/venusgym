d43552ff3cbbda72108534884baf7035751c8029
squares2 <strategyquant8@gmail.com>
2025-09-30 23:37:45 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-<script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
@@ -18,4 +17,3 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   alert("gd");
-</script>
