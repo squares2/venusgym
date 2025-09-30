b5ff45ee2fc4af4114cbaacc2f26e96876c6e27d
squares2 <strategyquant8@gmail.com>
2025-09-30 23:44:19 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyAVz09X6t6h4AnIUyl8CSpHNWgsLwjAqRk",
     authDomain: "venusgym-5c509.firebaseapp.com",
@@ -13,7 +8,5 @@
     messagingSenderId: "694873396897",
     appId: "1:694873396897:web:ea68b2d124070a09ac42e0"
   };
-
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   alert("gd");
