4c93d1670d404fc217bc9b9059aece9bee1cad88
squares2 <strategyquant8@gmail.com>
2025-09-30 14:30:34 +0300

    Add files via upload

---
+++ table.js
@@ -0,0 +1,16 @@
+const table = document.querySelector('table')
+const allCells = [... document.querySelectorAll('td')]
+const mainCells = [... document.querySelectorAll('.row-1 td')]
+
+function handleMouseover(cell) {
+  cell.addEventListener('mouseover', () => {
+    mainCells.some(mainCell => {
+      mainCell.classList.remove('on-hover')
+      mainCell.classList.contains(cell.classList) && mainCell.classList.add('on-hover')
+    })
+  })
+}
+
+allCells.forEach(handleMouseover)
+
+table.addEventListener('mouseleave', () => mainCells.forEach(mainCell => mainCell.classList.remove('on-hover')))
